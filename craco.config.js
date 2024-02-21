5f960cb3ae191473a498b05bfe05dba0e1ad0f8f
jungyu333 <jungyu3826@gmail.com>
2024-02-21 15:50:15 +0900

    [Chore] Setting Craco & Prettier

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+const CracoAlias = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: "./src",
+        tsConfigPath: "tsconfig.paths.json",
+      },
+    },
+  ],
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      if (env === "production") {
+        webpackConfig.resolve.alias = {
+          ...webpackConfig.resolve.alias,
+        };
+      }
+      return webpackConfig;
+    },
+  },
+};
