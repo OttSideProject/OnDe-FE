6bf223537ab4f25dcc452c5855b66d18b9ce450f
jungyu333 <jungyu3826@gmail.com>
2024-02-21 16:11:02 +0900

    [Chore] Setting react-query

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,21 @@ import App from './App';
 import GlobalStyle from '@styles/core/global';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@styles/core';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </>,
 );
