8f496db90ab96bb2f5229fd715a1ed732defb881
keemewoon <keemewoon@gmail.com>
2024-04-24 15:56:09 +0900

    emotion으로 변경

--- src/page/landing/Landing.tsx
+++ src/page/landing/Landing.tsx
@@ -1,3 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+const divStyle = css`
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  padding: 32px;
+  text-align: center;
+  &:hover {
+    color: white;
+  }
+`
+
 export const Landing = () => {
-  return <>Landing Page</>;
+  return <div css={divStyle}>Hover to change color.</div>
 };
