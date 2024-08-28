1e7a9931461c945025700ca98bbda3b428c7884a
yoonucho <tomorrowcho@gmail.com>
2024-08-28 19:25:33 +0900

    Refactor: global.ts 파일을 global.css로 대체

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,16 +1,6 @@
 // app/_app.tsx
 import type { AppProps } from 'next/app';
 
-import GlobalStyles from '@/components/GlobalStyles';
-
-import { ThemeProvider } from '@emotion/react';
-import theme from '@/styles/core/theme';
-
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
