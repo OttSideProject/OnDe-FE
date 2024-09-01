0ce2850ffaa2113d2fb867d042ae44eac6aaa8d7
BOOK-7HF2F58CR4\sour1 <wonderwall00@naver.com>
2024-09-01 23:50:15 +0900

    Feat : 게시판 메인화면 주간베스트글까지 UI완성(API연동 아직)

---
+++ src/app/board/page.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import { PostDetailType, UserInfo } from '@/_types/board/board';
+import WeeklyPostListItem from '@/components/board/WeeklyPostListItem';
+import Header from '@/components/header/Header';
+import {
+  MainPageWrapper,
+  CategoryWrapper,
+  Container,
+  BoardTitle,
+  CategoryTitle,
+  WeeklyBestPostContainer,
+  NullPost,
+  WeeklyBestPostNumber,
+  WeeklyBestPostWrapper,
+  PostDetailWrapper,
+  PostTitle,
+  PostContents,
+  PostDetailContainer,
+} from '@/styles/board/main';
+import signup from '@/styles/user/signup';
+import axios from 'axios';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+
+const page = () => {
+  const [selectedCategory, setSelectedCategory] = useState('홈');
+  const [weeklyBestPost, setWeeklyBestPost] = useState<PostDetailType[]>([]);
+  // const [userInfos, setUserInfos] = useState<UserInfo[]>([]);
+
+  const handleSelectedCategory = useCallback((category: string) => {
+    setSelectedCategory(category);
+  }, []);
+
+  const categoryTitle = ['홈', '후기', '자유톡', 'Q&A'].map((title, idx) => {
+    return (
+      <CategoryTitle
+        key={idx}
+        selectedCategory={selectedCategory}
+        onClick={() => handleSelectedCategory(title)}
+      >
+        {title}
+      </CategoryTitle>
+    );
+  });
+
+  // FIXME : 선택 카테고리에 따라서 api 요청을 다르게
+  const getWeeklyBestPost = useCallback(() => {
+    setWeeklyBestPost([
+      {
+        post_idx: 6,
+        user_idx: 1,
+        title: '제목입니다',
+        contents:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis',
+        post_views: 0,
+        boardid: 1,
+        like_count: 0,
+        createdAt: '2024-07-21T13:34:29',
+        modifiedAt: '2024-07-21T13:34:29',
+      },
+      {
+        post_idx: 7,
+        user_idx: 2,
+        title: '제목입니다2',
+        contents:
+          'Senectus et netus et malesuada. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Neque convallis a cras semper auctor.',
+        post_views: 0,
+        boardid: 1,
+        like_count: 0,
+        createdAt: '2024-07-21T13:34:29',
+        modifiedAt: '2024-07-21T13:34:29',
+      },
+      {
+        post_idx: 7,
+        user_idx: 4,
+        title: '제목입니다3',
+        contents: '본문입니다',
+        post_views: 0,
+        boardid: 1,
+        like_count: 0,
+        createdAt: '2024-07-21T13:34:29',
+        modifiedAt: '2024-07-21T13:34:29',
+      },
+    ]);
+  }, []);
+
+  useEffect(() => {
+    getWeeklyBestPost();
+  }, []);
+
+  return (
+    <MainPageWrapper>
+      <Container>
+        <signup.Header>
+          <signup.HeaderInner>게시판</signup.HeaderInner>
+        </signup.Header>
+        <CategoryWrapper>{categoryTitle}</CategoryWrapper>
+        <BoardTitle>주간 베스트 글</BoardTitle>
+        <WeeklyBestPostContainer>
+          {weeklyBestPost.length === 0 ? (
+            <NullPost>게시글이 아직 없습니다!</NullPost>
+          ) : (
+            <PostDetailContainer>
+              {weeklyBestPost.map((post, idx) => (
+                <WeeklyPostListItem
+                  key={idx}
+                  post={post}
+                  user_idx={post.user_idx}
+                  idx={idx}
+                ></WeeklyPostListItem>
+              ))}
+            </PostDetailContainer>
+          )}
+        </WeeklyBestPostContainer>
+      </Container>
+    </MainPageWrapper>
+  );
+};
+
+export default page;
---
+++ src/styles/board/main.ts
@@ -0,0 +1,151 @@
+'use client';
+import styled from '@emotion/styled';
+
+interface CategoryTitleProps {
+  selectedCategory: string;
+  onClick: () => void;
+}
+
+export const MainPageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  min-width: 320px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const CategoryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  text-align: center;
+  box-sizing: border-box;
+  padding: 10px 0px 16px 0px;
+  height: 74px;
+  line-height: 48px;
+  font-size: 16px;
+`;
+
+export const CategoryTitle = styled.div<CategoryTitleProps>`
+  cursor: pointer;
+  color: ${(props) =>
+    props.selectedCategory === props.children ? '#F2F2F2' : '#4F4F4F'};
+  border-bottom: ${(props) =>
+    props.selectedCategory === props.children
+      ? '0.2rem solid #A4E600'
+      : '0rem'};
+`;
+
+export const BoardTitle = styled.div`
+  box-sizing: border-box;
+  padding-left: 10px;
+  height: 39px;
+  line-height: 39px;
+  font-size: 16px;
+`;
+
+export const WeeklyBestPostContainer = styled.div`
+  display: flex;
+  flex-direction: colunm;
+  width: 100%;
+  font-size: 1.6rem;
+`;
+
+export const WeeklyBestPostWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 1.3rem 1rem 1.3rem 1rem;
+  border-bottom: 0.1rem solid #212121;
+`;
+
+export const DirectionWrapper = styled.div`
+  display: flex;
+`;
+
+export const PostDetailContainer = styled.div`
+  width: 100%;
+`;
+
+export const PostDetailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20.8rem;
+  height: 4rem;
+  cursor: pointer;
+`;
+
+export const PostTitle = styled.div`
+  font-weight: 600;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const PostContents = styled.div`
+  color: #d1d1d1;
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 1.671rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding-top: 0.7rem;
+`;
+
+export const NullPost = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 18.4rem;
+`;
+
+export const WeeklyBestPostNumber = styled.div`
+  display: inline;
+  color: #bfff00;
+  font-weight: 700;
+  line-height: 1.909rem;
+  padding: 1rem 1rem 1rem 0rem;
+`;
+
+export const UserProfileImg = styled.img`
+  width: 1.6rem;
+  height: 1.6rem;
+  border-radius: 70%;
+  box-sizing: border-box;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.2rem;
+  color: #6d6d6d;
+  box-sizing: border-box;
+  padding: 1rem 0 0 2rem;
+`;
+
+export const UserName = styled.div`
+  margin: 0 0.6rem 0 0.6rem;
+`;
+
+export const UserInfoText = styled.div`
+  display: flex;
+`;
+export const PostInfoText = styled.div`
+  display: flex;
+  line-height: 1.432rem;
+`;
+
+export const LikeCountText = styled.div`
+  margin: 0.1rem 0.4rem 0.1rem 0.4rem;
+`;
+
+export const CommentCountText = styled.div`
+  margin: 0.1rem 0.4rem 0.1rem 0.4rem;
+`;
