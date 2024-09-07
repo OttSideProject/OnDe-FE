88e2219737eae97c8e9efba9754bb10650043c34
yoonucho <tomorrowcho@gmail.com>
2024-09-08 03:54:08 +0900

    Refactor:  SectionSliderContainer 컴포넌트를 생성후 코드 분리후 page에 임포트

--- src/components/contents/SectionSlider.tsx
+++ src/components/contents/SectionSlider.tsx
@@ -4,24 +4,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import Slider from 'react-slick';
-import Button from '../shared/button-group/Button';
+
+import ActionBar from '../shared/action-bar/ActionBar';
+
+import { SectionSliderProps } from '@/_types/contents/contents';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from './SectionSlider.module.css';
 
-/* 슬라이드를 위한 타입 */
-export type SectionSlide = {
-  id: number;
-	url: string;
-};
-
-/* 슬라이더 컴포넌트를 위한 타입 */
-export type SectionSliderProps = {
-	sectionSlides: SectionSlide[];
-}
-
-const SectionSlider: React.FC<SectionSliderProps> = ({ sectionSlides }) => {
+const SectionSlider: React.FC<SectionSliderProps> = ({
+  sectionSlides,
+  showActionBar,
+}) => {
   const settings = {
     infinite: true,
     speed: 500,
@@ -36,50 +31,25 @@ const SectionSlider: React.FC<SectionSliderProps> = ({ sectionSlides }) => {
       <div className={`${styles.slider} section-slider`}>
         <Slider {...settings}>
           {sectionSlides.map((sectionSlide, index) => (
-            <Link
-              key={index}
-              href={sectionSlide.url}
-              className={styles.cardLink}
-            >
-              <figure
-                className={`${styles.slide} ${
-                  !sectionSlide.url ? styles.emptySlide : ''
-                }`}
-              >
-                {sectionSlide.url && (
-                  <Image
-                    src={sectionSlide.url}
-                    alt={`Slide ${sectionSlide.id} ${index}`}
-                    width={104}
-                    height={156}
-                  />
-                )}
-                <div
-                  className={styles.btnContainer}
-                  style={{ display: 'none' }}
+            <>
+              <Link key={index} href="#" className={styles.cardLink}>
+                <figure
+                  className={`${styles.slide} ${
+                    !sectionSlide.url ? styles.emptySlide : ''
+                  }`}
                 >
-                  {/* 첫 번째 버튼 */}
-                  <Button
-                    variant="primary"
-                    size="small"
-                    text="First Button"
-                    onClick={() => alert('First Button Clicked!')}
-                  >
-                    First Button
-                  </Button>
-
-                  {/* 두 번째 버튼 */}
-                  <Button
-                    variant="secondary"
-                    size="small"
-                    text="Second Button"
-                    onClick={() => alert('Second Button Clicked!')}
-                  >
-                    Second Button
-                  </Button>
-                </div>
-              </figure>
-            </Link>
+                  {sectionSlide.url && (
+                    <Image
+                      src={sectionSlide.url}
+                      alt={`Slide ${sectionSlide.id} ${index}`}
+                      width={104}
+                      height={156}
+                    />
+                  )}
+                </figure>
+                {showActionBar && sectionSlide.url && <ActionBar />}
+              </Link>
+            </>
           ))}
         </Slider>
       </div>
---
+++ src/components/contents/SectionSliderContainer.tsx
@@ -0,0 +1,225 @@
+'use client';
+import { useEffect } from 'react';
+/* Components */
+import SubHeader from '@/components/contents/header/SubHeader';
+import SectionSlider from '@/components/contents/SectionSlider';
+import { DimmedBackground } from '@/components/shared/DimmedBackground';
+/* Types */
+import { Section } from '@/_types/contents/contents';
+/* Zustand store */
+import useDropDownStore from '@/stores/useDropDownStore';
+import DropDownOptions from '../shared/action-bar/DropDownOptions';
+
+const sections: Section[] = [
+  {
+    id: 1,
+    title: '님이 즐겨찾는 콘텐츠',
+    linkText: '모아보기 더보기',
+    sectionSlides: [
+      {
+        id: 1,
+        url: 'https://picsum.photos/104/156?random=1',
+      },
+      {
+        id: 2,
+        url: 'https://picsum.photos/104/156?random=2',
+      },
+      {
+        id: 3,
+        url: '',
+      },
+      {
+        id: 4,
+        url: '',
+      },
+      {
+        id: 5,
+        url: '',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: '소년만화 좋아하시죠?',
+    linkText: '추천 더보기',
+    sectionSlides: [
+      {
+        id: 1,
+        url: 'https://picsum.photos/104/156?random=1',
+      },
+      {
+        id: 2,
+        url: 'https://picsum.photos/104/156?random=2',
+      },
+      {
+        id: 3,
+        url: 'https://picsum.photos/104/156?random=3',
+      },
+      {
+        id: 4,
+        url: 'https://picsum.photos/104/156?random=4',
+      },
+      {
+        id: 5,
+        url: 'https://picsum.photos/104/156?random=5',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'NEW! 따끈따끈한 신작',
+    linkText: '신작 더보기',
+    sectionSlides: [
+      {
+        id: 1,
+        url: 'https://picsum.photos/104/156?random=1',
+      },
+      {
+        id: 2,
+        url: 'https://picsum.photos/104/156?random=2',
+      },
+      {
+        id: 3,
+        url: 'https://picsum.photos/104/156?random=3',
+      },
+      {
+        id: 4,
+        url: 'https://picsum.photos/104/156?random=4',
+      },
+      {
+        id: 5,
+        url: 'https://picsum.photos/104/156?random=5',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: '지금 가장 인기있는 시리즈',
+    linkText: '드라마 랭킹 더보기',
+    sectionSlides: [
+      {
+        id: 1,
+        url: 'https://picsum.photos/104/156?random=1',
+      },
+      {
+        id: 2,
+        url: 'https://picsum.photos/104/156?random=2',
+      },
+      {
+        id: 3,
+        url: 'https://picsum.photos/104/156?random=3',
+      },
+      {
+        id: 4,
+        url: 'https://picsum.photos/104/156?random=4',
+      },
+      {
+        id: 5,
+        url: 'https://picsum.photos/104/156?random=5',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: '지금 가장 인기있는 영화',
+    linkText: '영화 랭킹 더보기',
+    sectionSlides: [
+      {
+        id: 1,
+        url: 'https://picsum.photos/104/156?random=1',
+      },
+      {
+        id: 2,
+        url: 'https://picsum.photos/104/156?random=2',
+      },
+      {
+        id: 3,
+        url: 'https://picsum.photos/104/156?random=3',
+      },
+      {
+        id: 4,
+        url: 'https://picsum.photos/104/156?random=4',
+      },
+      {
+        id: 5,
+        url: 'https://picsum.photos/104/156?random=5',
+      },
+    ],
+  },
+];
+
+const userName = '디미';
+
+const SectionSliderContainer: React.FC = () => {
+  const { isDropDownOpen, openDropDown, closeDropDown } = useDropDownStore();
+
+  useEffect(() => {
+    if (isDropDownOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isDropDownOpen]);
+
+  const options = [
+    { id: 1, label: '에피소드 및 정보', url: '/assets/images/icons/info.svg' },
+    { id: 2, label: '모아보기', url: '/assets/images/icons/collect-box.svg' },
+    {
+      id: 3,
+      label: '모아보기 삭제',
+      url: '/assets/images/icons/iconamoon_close-light.svg',
+    },
+    { id: 4, label: '에피소드 및 정보', url: '/assets/images/icons/info.svg' },
+    { id: 5, label: '모아보기', url: '/assets/images/icons/collect-box.svg' },
+    {
+      id: 6,
+      label: '모아보기 삭제',
+      url: '/assets/images/icons/iconamoon_close-light.svg',
+    },
+    { id: 7, label: '에피소드 및 정보', url: '/assets/images/icons/info.svg' },
+    { id: 8, label: '모아보기', url: '/assets/images/icons/collect-box.svg' },
+    {
+      id: 9,
+      label: '모아보기 삭제',
+      url: '/assets/images/icons/iconamoon_close-light.svg',
+    },
+  ];
+
+  const handleOptionSelect = (id: number) => {
+    console.log('선택된 옵션:', id, options[id - 1].label);
+    closeDropDown();
+  };
+
+  return (
+    <>
+      {/* DropDown이 열려있을 때 DimmedBackground 표시 */}
+      {isDropDownOpen && (
+        <>
+          <DimmedBackground onClick={closeDropDown} />
+          <DropDownOptions options={options} onSelect={handleOptionSelect} />
+        </>
+      )}
+      {/* SectionSlider */}
+      {sections.map((section) => (
+        <div key={section.id}>
+          {/* 첫 번째 섹션일 때 사용자 이름을 추가 */}
+          <SubHeader
+            title={
+              section.id === 1 ? `${userName}${section.title}` : section.title
+            }
+            linkText={section.linkText}
+          />
+          <SectionSlider
+            sectionSlides={section.sectionSlides}
+            showActionBar={section.id === 1}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SectionSliderContainer;
