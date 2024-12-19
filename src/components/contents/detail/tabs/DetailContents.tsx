ecf7074da2ba9575d74790dd40d63db83ebeef05
yoonucho <tomorrowcho@gmail.com>
2024-12-20 03:37:57 +0900

    Styles: 상세페이지 하단 탭 ON생각 커밍순 이미지 추가

--- src/components/contents/detail/tabs/DetailContents.tsx
+++ src/components/contents/detail/tabs/DetailContents.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-
+import Image from 'next/image';
 import Tabs from '@/components/shared/tabs/Tabs';
 import ViewMoreButton from '@/components/shared/view-more/ViewMoreButton';
 
@@ -12,16 +12,12 @@ const DetailContents = () => {
   const categories = [
     { label: '상세정보', key: 'info' },
     {
-      label: (
-        <span>
-          한마디 <span className="number-font">({commentsCount})</span>
-        </span>
-      ),
+      label: <span>ON생각</span>,
       key: 'comment',
     },
   ];
 
-  const detailInfo = `출연: 도하석, 김예운, 정예원, 조윤우, 윤수오, 고민주, 신혜원
+  const detailInfo = `출연: 도하석, 김예운, 조윤우, 고민주, 신혜원
 					감독: 피트 닥터
 					타입: 애니메이션, 영화
 					장르: 애니, 모험
@@ -44,8 +40,18 @@ const DetailContents = () => {
         );
       case 'comment':
         return (
-          <div>
-            <p>한마디 내용이 여기에 표시됩니다.</p>
+          <div className="coming-soon">
+            <Image
+              src="/assets/images/coming-soon.png"
+              alt="on생각 준비중"
+              sizes="100vw"
+              width={500}
+              height={670}
+              style={{
+                width: '100%',
+                height: 'auto',
+              }}
+            />
           </div>
         );
       default:
