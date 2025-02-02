12563b52bc44aaab71b09f996872623b994a709e
yoonucho <tomorrowcho@gmail.com>
2025-02-02 15:24:08 +0900

    Feat: fetchRankings 함수에 로깅 기능을 추가하여 디버깅 및 응답 추적 개선

--- src/api/fetchRankings.ts
+++ src/api/fetchRankings.ts
@@ -3,15 +3,20 @@ import { fetchers } from '@/__mocks__/helpers/fetchers';
 import { RankingsResponse } from '@/_types/contents/contents';
 
 export const fetchRankings = async (
-	type: string,
+  type: string,
   ott: string,
   id: number,
 ): Promise<RankingsResponse> => {
   try {
+    console.log('Fetching Rankings:', { type, ott, id }); // 요청 매개변수 출력
     if (process.env.NEXT_PUBLIC_API_MOCKING === 'true') {
+			console.log('Using MSW Mocking...');
       // MSW를 사용하여 API mocking을 하는 경우, fetchSections 함수를 사용할 때 아래 코드가 실행됩니다.
-      return await fetchers.fetchRankings(type, ott, id);
+      const response= await fetchers.fetchRankings(type, ott, id);
+			console.log('Mock Response:', response);
+			return response;
     } else {
+			console.log('Using Real API...');
       // 실제 API 호출
       const response = await Api.get<RankingsResponse>(
         `/contents/ranking/ott`,
@@ -19,7 +24,10 @@ export const fetchRankings = async (
           baseURL: process.env.NEXT_PUBLIC_BASE_END_POINT,
           params: { type, ott, id },
         },
+				
       );
+
+      console.log('API Response:', response.data); // 응답 확인
       return response.data;
     }
   } catch (error) {
--- src/components/MSWInitializer.tsx
+++ src/components/MSWInitializer.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 const MSWInitializer = () => {
   useEffect(() => {
     if (
-      process.env.NODE_ENV === 'development' &&
+      // process.env.NODE_ENV === 'development' &&
       process.env.NEXT_PUBLIC_API_MOCKING === 'true'
     ) {
       const startWorker = async () => {
