c5542f37c13d102c8309471ac0b2173d17088e19
yoonucho <tomorrowcho@gmail.com>
2025-02-28 03:31:51 +0900

    Feat: useImageMapping 훅 추가 및 이미지 매핑 기능 구현

---
+++ src/entities/contents/hooks/useImageMapping.ts
@@ -0,0 +1,23 @@
+'use client';
+
+import { useCallback } from 'react';
+import imageMapping from '@/features/shared/utils/imageMapping';
+
+type PageType = 'contentMain' | 'ranking' | 'recommended';
+
+/**
+ * 이미지 매핑을 위한 훅
+ * @returns {Object} getImageSrc 함수를 포함한 객체
+ * @example
+ * const { getImageSrc } = useImageMapping();
+ * const imageSrc = getImageSrc('제목', 'contentMain');
+ */
+const useImageMapping = () => {
+  const getImageSrc = useCallback((title: string, pageType: PageType) => {
+    return imageMapping[pageType]?.[title] || '';
+  }, []);
+
+  return { getImageSrc };
+};
+
+export default useImageMapping;
