59945e98ee95c16ade7e76e4c234677385dfece7
yoonucho <tomorrowcho@gmail.com>
2025-03-24 13:09:07 +0900

    Feat: 메타데이터에 OpenGraph와 키워드 정보 추가 - SEO 최적화를 위한 키워드 메타데이터 추가 - 소셜 미디어 공유를 위한 OpenGraph 메타데이터 구현 - 사이트 정보와 이미지 URL 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,23 @@ export const metadata: Metadata = {
   description:
     '쉽고 간편한 검색과 다양한 추천 서비스를 제공하는 OTT 정보 플랫폼 서비스입니다. ',
   icons: '/favicon.svg',
+  keywords: [
+    'ONDE',
+    'OND',
+    '온 세상 모든 온-디',
+    '온디',
+    'OTT 큐레이션',
+    'ott 정보 사이트',
+  ],
+  openGraph: {
+    title: '당신이 필요한 콘텐츠를 ON!',
+    description:
+      '쉽고 간편한 검색과 다양한 추천 서비스를 제공하는 OTT 정보 플랫폼 서비스입니다.',
+    siteName: 'OnD',
+    images: { url: '/assets/images/ond-open-graph.png' },
+    url: 'https://www.ondemandia.com/contents/main',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
